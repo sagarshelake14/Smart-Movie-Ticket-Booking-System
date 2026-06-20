81726b6f67e92b5bc43dfe9750c21de8ab063756
sagarshelake14 <sagarshelake0014@gmail.com>
2026-06-20 12:47:19 +0530

    creating register screen part 1

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,10 +1,41 @@
 import React from "react";
-import { Button } from 'antd';
+import { Button, Form } from 'antd';
 
 function Register(){
          return (
-                  <div className="flex justify-center h-screen item-center">
-                           <Button type="primary">Primary Button</Button>
+                  <div className="flex justify-center h-screen item-center bg-primary">
+                           <div className="card p-3 w-400">
+                                    <h1 className="text-xl">MOVIEMAGIC - REGISTER</h1>
+                                    <hr/>
+                                    <Form
+                                    layout="vertical" 
+                                    className="mt-1"
+                                    >
+                                             <Form.Item 
+                                             label="Name"
+                                             name="name"
+                                             rules={[{required: true, message:'please input your name'}]}
+                                             >
+                                                      <input type="text"/>
+                                             </Form.Item>
+
+                                             <Form.Item 
+                                             label="Email"
+                                             name="email"
+                                             rules={[{required: true, message:'please input your email'}]}
+                                             >
+                                                      <input type="email"/>
+                                             </Form.Item>
+
+                                             <Form.Item 
+                                             label="Password"
+                                             name="password"
+                                             rules={[{required: true, message:'please input your password'}]}
+                                             >
+                                                      <input type="password"/>
+                                             </Form.Item>
+                                    </Form>
+                           </div>
                   </div>
          )
 }
