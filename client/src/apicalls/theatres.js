2fc3395621001eebe387075f565bb298e5879b9e
sagarshelake14 <sagarshelake0014@gmail.com>
2026-07-08 23:00:41 +0530

    theatres list from movie API

--- client/src/apicalls/theatres.js
+++ client/src/apicalls/theatres.js
@@ -99,6 +99,19 @@ export const DeleteShow = async (payload) => {
     }
 };
 
+// get all theatres for a movie
+export const GetAllTheatresByMovie = async (payload) => {
+    try {
+        const response = await axiosInstance.post(
+            "/api/theatres/get-all-theatres-by-movie",
+            payload
+        );
+        return response.data;
+    } catch (error) {
+        return error.response;
+    }
+};
+
 
 // import { axiosInstance } from ".";
 
